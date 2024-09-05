ddf65e64f7b193c32bf4314d36285d16c91442d2
Yeltay Bayandinov <bayandinov13@gmail.com>
2024-09-05 13:30:52 +0500

    Fixed UI pricing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  site: "https://yeltay13.github.io",
+  site: "https://y-boost.github.io",
   base: "y-boost",
 });
