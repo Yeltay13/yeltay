d441e8c5d18741121ae51ad9488e8496a398e144
Yeltay Bayandinov <bayandinov13@gmail.com>
2024-09-08 15:28:59 +0500

    Added carousel to testimonials

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,4 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  site: "https://yeltay13.github.io",
-  base: "y-boost",
 });
