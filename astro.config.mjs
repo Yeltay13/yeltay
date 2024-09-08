9bcd8086805750b7aa825367b6744184a66c1a98
Yeltay Bayandinov <bayandinov13@gmail.com>
2024-09-08 16:02:42 +0500

    Changed README.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/y-boost/", // Set this to your repo name
   integrations: [tailwind(), icon()],
 });
